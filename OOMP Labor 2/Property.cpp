46dd34435e10436765bc9d099d11c3d6369fb432
Marius Grubel <m.grubel@ostfalia.de>
2021-04-23 16:09:23 +0200

    Property

--- OOMP Labor 2/Property.cpp
+++ OOMP Labor 2/Property.cpp
@@ -1,1 +1,9 @@
 #include "Property.h"
+
+int Property::get_Rent()
+{
+}
+
+void Property::enter(player : Player&, int fields_to_go)
+{
+}
--- OOMP Labor 2/Property.h
+++ OOMP Labor 2/Property.h
@@ -1,7 +1,51 @@
 #pragma once
+#include <string>
 #include "Field.h"
+#include "Player.h"
+#include "Monopoly.h"
+
 class Property :
-    public Field
-{
+    public Field {
+private:
+    Player* owner;
+    int value;
+    Monopoly* monopoly;
+
+public:
+    int get_Rent();
+    void enter(player:Player&, int fields_to_go);
+
+    Property();
+    ~Property();
+
+};
+
+class Railroad :
+       public Property {
+private:
+    int rent[4];
+
+public:
+    int get_rent();
+};
+
+class Street :
+    public Property {
+private:
+    int rent[6];
+    int houses;
+
+public:
+    int get_rent();
+};
+
+class Utility :
+    public Property {
+private: 
+    int dice_factor_single;
+    int dice_factor_monopoly;
+
+public:
+    int get_rent();
 };
 
