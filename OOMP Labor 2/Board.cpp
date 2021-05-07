9992fd5dac3c20d683bd8c39132ec6213b0cc85c
Jeremias Diekamp <j.diekamp@ostfalia.de>
2021-05-07 19:09:58 +0200

    Felder zuende angelegt

--- OOMP Labor 2/Board.cpp
+++ OOMP Labor 2/Board.cpp
@@ -31,7 +31,7 @@ Board::Board(int iPlayer)
 	Board::vBoard.push_back(tTax1);
 	Field* rBahn1 = new Railroad("Suedbahnhof", mBahn);
 	Board::vBoard.push_back(rBahn1);
-	Field* gef1 = new GoToJail("Gefaengnis");
+	Field* gef1 = new ActionField("Gefaengnis");
 	Board::vBoard.push_back(gef1);
 	Field* sMuen = new Street("Muenchenerstrasse", 180, mOrange, 14, 70, 200, 550, 750, 950);
 	Board::vBoard.push_back(sMuen);
@@ -47,6 +47,32 @@ Board::Board(int iPlayer)
 	Board::vBoard.push_back(uElek);
 	Field* naNoPark = new NoAction(0, "Frei Parken");
 	Board::vBoard.push_back(naNoPark);
+	Field* aGem3 = new ActionField("Gemeinschaftsfeld");
+	Board::vBoard.push_back(aGem3);
+	Field* sLess = new Street("Lessingstrasse", 260, mGelb, 22, 110, 330, 800, 975, 1150);
+	Board::vBoard.push_back(sLess);
+	Field* sSchi = new Street("Schillerstrasse", 260, mGelb, 22, 110, 330, 800, 975, 1150);
+	Board::vBoard.push_back(sSchi);
+	Field* uWass = new Utility("Wasserwerk", mVers);
+	Board::vBoard.push_back(uWass);
+	Field* sGoet = new Street("Goethestrasse", 280, mGelb, 24, 120, 360, 850, 1025, 1200);
+	Board::vBoard.push_back(sGoet);
+	Field* rBahn3 = new Railroad("Nordbahnhof", mBahn);
+	Board::vBoard.push_back(rBahn3);
+	Field* gGTJ = new GoToJail("Gehe ins Gefaengnis", gef1);
+	Board::vBoard.push_back(gGTJ);
+	Field* sPark = new Street("Parkstrasse", 350, mBlau, 35, 175, 500, 1100, 1300, 1500);
+	Board::vBoard.push_back(sPark);
+	Field* aGem4 = new ActionField("Ereignisfeld");
+	Board::vBoard.push_back(aGem4);
+	Field* sSchl = new Street("Schlossallee", 400, mBlau, 50, 200, 600, 1400, 1700, 2000);
+	Board::vBoard.push_back(sSchl);
+	Field* tTax2 = new Tax(100,"Zusatzsteuer");
+	Board::vBoard.push_back(tTax2);
+	Field* rBahn4 = new Railroad("Haupbahnhof", mBahn);
+	Board::vBoard.push_back(rBahn4);
+
+
 
 
 
