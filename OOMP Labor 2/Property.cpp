f4e99d88cd0973af2fbaadb9683de780e26a82dd
Jonas Fasterling <67583771+J-Fasterling@users.noreply.github.com>
2021-05-10 22:46:11 +0200

    farige Ausgaben

--- OOMP Labor 2/Property.cpp
+++ OOMP Labor 2/Property.cpp
@@ -16,10 +16,19 @@ void Property::enter(Player& player)
 
 	if (owner)
 	{
+		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 		player.setMoney(player.getMoney() - get_rent());
 		owner->setMoney(owner->getMoney() + get_rent());
-		std::cout << player.get_Name() << " bezahlt " << get_rent() << "$ miete an " << owner->get_Name() << std::endl;
-		std::cout << owner->get_Name() << " bekommt " << get_rent() << "$ an Miete von " << player.get_Name() << std::endl;
+		std::cout << player.get_Name() << " bezahlt ";
+		SetConsoleTextAttribute(hConsole, 12);
+		std::cout << get_rent() << "$ ";
+		SetConsoleTextAttribute(hConsole, 15);
+		std::cout << "miete an " << owner->get_Name() << std::endl;
+		std::cout << owner->get_Name() << " bekommt "; 
+		SetConsoleTextAttribute(hConsole, 10);
+		std::cout << get_rent() << "$ ";
+		SetConsoleTextAttribute(hConsole, 15);
+		std::cout << "an Miete von " << player.get_Name() << std::endl;
 	}
 	else
 	{
