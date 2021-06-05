6d655228aa9117e26626e3f4bf88c9b0d6162408
Marius Grubel <m.grubel@ostfalia.de>
2021-06-05 11:42:28 +0200

    Rueckgängig ALAAArm

--- OOMP Labor 2/Field.h
+++ OOMP Labor 2/Field.h
@@ -20,14 +20,11 @@ private:
 protected:
 	Board* board;
 
-
 public:
 	Field(std::string _name, Field* _next = NULL, Field* _prev = NULL);
 	~Field();
 
-	friend class Board;
 	Board* board;
-	//friend class Board;
 	//Ausgabe, welcher Spieler welches Feld betreten hat
 	virtual void enter(Player& player) = 0;
 
